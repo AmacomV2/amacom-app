eb00d610482022c8979d72624169c2f2c5130787
Mario Vallejo <mario.vallejo@imagineapps.co>
2023-07-05 20:48:52 -0500

    feat: :fire: Hello
    Helow world

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Amacom init'),
+      home: const MyHomePage(title: 'Amacom init 2'),
     );
   }
 }
